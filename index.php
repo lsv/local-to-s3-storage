c6a85d27e214f45289cc1f6d7d4fcd7813a82db1
Martin Aarhof <martin.aarhof@gmail.com>
2019-10-03 12:17:03 +0200

    changed from local to sftp

--- index.php
+++ index.php
@@ -2,42 +2,51 @@
 
 use App\Command\CopyCommand;
 use Aws\S3\S3Client;
-use League\Flysystem\Adapter\Local;
 use League\Flysystem\AwsS3v3\AwsS3Adapter;
 use League\Flysystem\Filesystem;
+use League\Flysystem\Sftp\SftpAdapter;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Dotenv\Dotenv;
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 $dotenv = new Dotenv(false);
 
-$envFile = __DIR__ . '/.env';
+$envFile = __DIR__.'/.env';
 if (!file_exists($envFile)) {
     throw new RuntimeException('.env file is not created');
 }
 
-$dotenv->load(__DIR__ . '/.env');
+$dotenv->load(__DIR__.'/.env');
 
-$localDirectory = __DIR__ . '/' . $_ENV['LOCAL_DIRECTORY'];
 $s3key = $_ENV['DO_SPACES_KEY'];
 $s3secret = $_ENV['DO_SPACES_SECRET'];
 $s3region = $_ENV['DO_SPACES_REGION'];
 $s3bucket = $_ENV['DO_SPACES_BUCKETNAME'];
 
-$sourceAdapter = new Local($localDirectory);
+$sourceAdapter = new SftpAdapter(
+    [
+        'host' => $_ENV['SFTP_HOST'],
+        'port' => $_ENV['SFTP_PORT'],
+        'username' => $_ENV['SFTP_USERNAME'],
+        'password' => $_ENV['SFTP_PASSWORD'],
+        'root' => $_ENV['SFTP_STARTDIR'],
+    ]
+);
 $sourceFs = new Filesystem($sourceAdapter);
 
-$targetClient = new S3Client([
-    'credentials' => [
-        'key'    => $s3key,
-        'secret' => $s3secret,
-    ],
-    'region' => $s3region,
-    'version' => 'latest',
-    'endpoint' => 'https://' . $s3region . '.digitaloceanspaces.com',
-]);
-$targetAdapter = new AwsS3Adapter($targetClient, $s3bucket);
+$targetClient = new S3Client(
+    [
+        'credentials' => [
+            'key' => $_ENV['DO_SPACES_KEY'],
+            'secret' => $_ENV['DO_SPACES_SECRET'],
+        ],
+        'region' => $_ENV['DO_SPACES_REGION'],
+        'version' => 'latest',
+        'endpoint' => 'https://'.$_ENV['DO_SPACES_REGION'].'.digitaloceanspaces.com',
+    ]
+);
+$targetAdapter = new AwsS3Adapter($targetClient, $_ENV['DO_SPACES_BUCKETNAME']);
 $targetFs = new Filesystem($targetAdapter);
 
 $app = new Application;
